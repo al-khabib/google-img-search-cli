7e7c1ad38b31eb13fe9f69815327d5584bfbc865
al-khabib <bahtiyorjonq777@gmail.com>
2026-03-25 21:02:57 +0100

    Write image insertion logic into collection

--- indexer.py
+++ indexer.py
@@ -3,8 +3,10 @@
 # Collection - like a table in a relational database
 
 import weaviate
+import os
 from weaviate.classes.config import Property, DataType
 from weaviate.collections.classes.config import Configure
+
 
 def main():
     client = weaviate.connect_to_local()
@@ -20,5 +22,24 @@ def main():
             #    tell the model which property to convert to vector
             vector_config=Configure.Vectors.img2vec_neural(image_fields=['image'])
         )
+
+        # get the images collection
+        img_collection = client.collections.get("Images")
+
+        image_folder = './images-to-index'
+
+        # look through the images in the image folder 
+        for filename in os.listdir(image_folder):
+            # get image path
+            image_path = os.path.join(image_folder, filename)
+
+            # insert this image into the collection
+            img_collection.data.insert(
+                {
+                    'filename': filename,
+                    'image': image_path # image_path is just a text, we need to encode the actual image data as a blob first
+                }
+            )
+
     except Exception as e:
         print(f"Collection already exists: {e}")    
